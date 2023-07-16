87ba3cc83118f82b49f70c2db4042532b943b488
Bipin <bk3284488@gmail.com>
2023-07-16 09:47:32 +0530

    added copy btn

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const joke = document.querySelector("#joke");
 const jokeBtn = document.querySelector("#jokeBtn");
+const copy = document.querySelector("#copy");
 const url =
   "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
 
 const generateJoke = async () => {
+  joke.classList.remove("fade");
   const setHeader = {
     headers: {
       Accept: "application/json",
@@ -12,7 +14,16 @@ const generateJoke = async () => {
   const res = await fetch(url, setHeader);
   const data = await res.json();
   joke.innerHTML = data.joke;
+  joke.classList.add("fade");
 };
 
 jokeBtn.addEventListener("click", generateJoke);
 generateJoke();
+copy.addEventListener("click", () => {
+  const text = joke.innerText;
+  navigator.clipboard.writeText(text);
+  copy.innerText = " Copied!";
+  setTimeout(() => {
+    copy.innerText = " Copy";
+  }, 1000);
+});
